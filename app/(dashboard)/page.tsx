738764e7942169ee8f8aa59fed3c9eff91901b0a
Amr Elshemy <amrrmohamed87@gmail.com>
2025-04-15 00:03:51 +0200

    Refactor Claims page to enhance layout and add export functionality

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,32 @@
-export default function Home() {
+import ClaimsWrapper from "@/components/claims/claims-wrapper";
+import { Button } from "@/components/ui/button";
+import { Download } from "lucide-react";
+
+export default function ClaimsAnalysis() {
   return (
-    <section className="p-4 w-full flex flex-col gap-5 overflow-x-hidden">
-      DOH AAP Payor Demo
+    <section className="w-full p-1 flex flex-col gap-5 overflow-x-hidden">
+      <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-4">
+        <div>
+          <h1 className="text-2xl font-semibold text-cyan-900 tracking-tight">
+            Claim Analysis
+          </h1>
+          <p className="text-gray-700/70 mt-1.5 font-medium">
+            Monitor claim-level activity across claims, identify high-impact
+            violations, and optimize claim performance over time
+          </p>
+        </div>
+
+        <Button
+          variant="outline"
+          size="lg"
+          className="flex items-center gap-2 text-cyan-700 border-cyan-200 hover:bg-cyan-50 transition-colors hover:cursor-pointer"
+        >
+          Export
+          <Download className="h-4 w-4" />
+        </Button>
+      </div>
+
+      <ClaimsWrapper />
     </section>
   );
 }
